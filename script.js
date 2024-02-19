0d9bb42aa6a8c3a6f44d61d562c95dcaa2b178a0
kcuevas <cuev73@live.com>
2024-02-19 13:48:14 -0500

    feature(hamburger menu): small adjustments to script.js and code cleanup in css

--- script.js
+++ script.js
@@ -1,41 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
+  {
+    const opener = document.querySelector('#main-menu-open');
+    document.addEventListener('keydown', (event) => {
+      if (event.key === 'Escape') opener.blur();
+    });
 
-  const opener = document.getElementById("main-menu-open");
-  document.addEventListener("keydown", (event) => {
-    if (event.key === "Escape") opener.blur();
-   
-  } );  
-  
-  opener.addEventListener("touchstart", () => {
-		opener.focus();
-	} );
-  opener.nextElementSibling.addEventListener("touchstart",  () => {
-		opener.blur();
-	} );
+    opener.addEventListener('click', () => {});
 
+    opener.addEventListener('touchstart', () => {
+      opener.focus();
+    });
+    opener.nextElementSibling.addEventListener('touchstart', () => {
+      opener.blur();
+    });
+  }
+  const reviewOneDialog = document.querySelector('#review-1-dialog');
+  const showDialogOne = document.querySelector('#open-review-1');
+  const closeDialogOne = document.querySelector('#close-review-1');
 
-const reviewOneDialog = document.querySelector("#review-1-dialog")
-const showDialogOne = document.querySelector("#open-review-1")
-const closeDialogOne = document.querySelector("#close-review-1")
+  showDialogOne.addEventListener('click', () => {
+    reviewOneDialog.showModal();
+  });
 
-showDialogOne.addEventListener("click", () => {
-  reviewOneDialog.showModal();
-})
+  closeDialogOne.addEventListener('click', () => {
+    reviewOneDialog.close();
+  });
+  const reviewTwoDialog = document.querySelector('#review-2-dialog');
+  const showDialogTwo = document.querySelector('#open-review-2');
+  const closeDialogTwo = document.querySelector('#close-review-2');
 
-closeDialogOne.addEventListener("click", () => {
-  reviewOneDialog.close();
-})
-const reviewTwoDialog = document.querySelector("#review-2-dialog")
-const showDialogTwo = document.querySelector("#open-review-2")
-const closeDialogTwo = document.querySelector("#close-review-2")
+  showDialogTwo.addEventListener('click', () => {
+    reviewTwoDialog.showModal();
+  });
 
-showDialogTwo.addEventListener("click", () => {
-  reviewTwoDialog.showModal();
-})
-
-closeDialogTwo.addEventListener("click", () => {
-  reviewTwoDialog.close();
-})
-
-
-})
+  closeDialogTwo.addEventListener('click', () => {
+    reviewTwoDialog.close();
+  });
+});
